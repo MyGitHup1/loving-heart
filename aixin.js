f443900555d3c81a9827afaac204bee61e391b26
Three stones <40292573+MyGitHup1@users.noreply.github.com>
2024-01-08 17:07:51 +0800

    Update aixin.js

--- aixin.js
+++ aixin.js
@@ -2,8 +2,8 @@
 // @name         鼠标点击爱心特效
 // @license      MIT
 // @namespace    http://tampermonkey.net/
-// @version      v0.0.0.4
-// @description  鼠标在页面点击会出现各种颜色的小心心111222333
+// @version      v0.0.0.5
+// @description  鼠标在页面点击会出现各种颜色的小心心111222333444
 // @author       You
 // @match        *://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
